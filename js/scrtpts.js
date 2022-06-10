7e67b26969a61848a04f43dbf831a528b1af5aee
Derrak Richard <derrak@gmail.com>
2022-06-10 10:31:06 -0700

    returns modified array with a string if specified value was found in an element

--- js/scrtpts.js
+++ js/scrtpts.js
@@ -3,16 +3,27 @@
 function beepBoop(userInputNumberString) {
   const userInt = parseInt(userInputNumberString,10);
   let numberRangeArray = [];
+  let boopedArray = [];
 
   for (let i=0; i <= userInt; i++) {
-    numberRangeArray.push(i);
-  }
-  return numberRangeArray;
+    numberRangeArray.push(i.toString());
+  };
+  
+  numberRangeArray.forEach(function(element) {
+    if (element.includes("3")) {
+      boopedArray.push("Won't you be my neighbor?");
+    }
+    else {
+      boopedArray.push(element);
+    };
+  });
   console.log(numberRangeArray);
+  console.log(boopedArray);
   };
 
 
 const userInputNumberString = "5";
-beepBoop(5);
+beepBoop(userInputNumberString);
 
 
+  
